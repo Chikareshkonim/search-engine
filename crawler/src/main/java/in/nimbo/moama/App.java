1047541cc941eb2d296a11b2abb56544df3b25b8
mousavi <mousavihosseini@ce.sharif.edu>
2018-08-26 02:15:42 +0430

    Created project structre

---
+++ crawler/src/main/java/in/nimbo/moama/App.java
@@ -0,0 +1,13 @@
+package in.nimbo.moama;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Crawler" );
+    }
+}
