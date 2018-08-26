40e29e76d1e8a458e42637090097c17de5d4a652
mousavi <mousavihosseini@ce.sharif.edu>
2018-08-26 19:13:21 +0430

    Added twitter package

--- news/src/main/java/in/nimbo/moama/App.java
+++ news/src/main/java/in/nimbo/moama/App.java
@@ -1,13 +1,20 @@
 package in.nimbo.moama;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "news" );
+public class App {
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("alireza").setMaster("spark://94.23.214.93:7077");
+        JavaSparkContext sparkContext = new JavaSparkContext(conf);
+        JavaRDD<Integer> rdd = sparkContext.parallelize(Arrays.asList(1, 2, 3, 4));
+        System.out.println(rdd.reduce((a, b) -> a + b));
     }
 }
---
+++ news/src/main/java/in/nimbo/moama/twitter/TwitterHandler.java
@@ -0,0 +1,47 @@
+package in.nimbo.moama.twitter;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.twitter.TwitterUtils;
+import twitter4j.Status;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class TwitterHandler {
+    private static Properties properties = new Properties();
+
+    static {
+        try {
+            properties.load(TwitterHandler.class.getResourceAsStream("/twitter.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private JavaStreamingContext jssc;
+
+    public TwitterHandler(String name, String master) {
+        SparkConf conf = new SparkConf().setAppName(name).setMaster(master).setJars(new String[]{"/home/mousavi/Desktop/searchengine/news/target/news-1.0-SNAPSHOT-jar-with-dependencies.jar"});
+        jssc = new JavaStreamingContext(conf, new Duration(3000));
+    }
+
+    public void run() {
+//        System.setProperties(properties);
+        System.setProperty("twitter4j.oauth.consumerKey", properties.getProperty("twitter4j.oauth.consumerKey"));
+        System.setProperty("twitter4j.oauth.consumerSecret", properties.getProperty("twitter4j.oauth.consumerSecret"));
+        System.setProperty("twitter4j.oauth.accessToken", properties.getProperty("twitter4j.oauth.accessToken"));
+        System.setProperty("twitter4j.oauth.accessTokenSecret", properties.getProperty("twitter4j.oauth.accessTokenSecret"));
+//        System.setProperty("twitter4j.http.proxyHost", "127.0.0.1");
+//        System.setProperty("twitter4j.http.proxyPort", "41709");
+        JavaReceiverInputDStream<Status> twitterStream = TwitterUtils.createStream(jssc);
+        JavaDStream<String> tweets = twitterStream.map(Status::getText);
+        tweets.print();
+        jssc.start();
+    }
+
+
+}
