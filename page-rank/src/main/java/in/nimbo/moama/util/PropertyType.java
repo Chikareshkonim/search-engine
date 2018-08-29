865bdac87df8b8306023976577a225cd06102800
amirsaeedr <amirsaeedranjbar@gmail.com>
2018-08-29 16:36:29 +0430

    add rank

--- page-rank/src/main/java/in/nimbo/moama/util/PropertyType.java
+++ page-rank/src/main/java/in/nimbo/moama/util/PropertyType.java
@@ -1,6 +1,6 @@
 package in.nimbo.moama.util;
 public enum PropertyType implements in.nimbo.moama.configmanager.PropertyType {
-    H_BASE_CONTENT_FAMILY("hbase.content.family"), H_BASE_TABLE("hbase.table"), HBASE_FAMILY_SCORE("hbase.family.score"),
+    H_BASE_CONTENT_FAMILY("hbase.family.outlinks"), H_BASE_TABLE("hbase.table"), HBASE_FAMILY_SCORE("hbase.family.score"),
     HBASE_REFRENCE_CLOUMN("hbase.refrence.column");
 
     public void setType(String type) {
