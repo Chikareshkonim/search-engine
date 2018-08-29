64bb4eea43734791a865b90b471c5762c7560d3c
mousavi <mousavihosseini@ce.sharif.edu>
2018-08-29 09:39:48 +0430

    Changed pom.xml

--- crawler/src/main/java/in/nimbo/moama/App.java
+++ crawler/src/main/java/in/nimbo/moama/App.java
@@ -2,9 +2,8 @@ package in.nimbo.moama;
 
 
 import in.nimbo.moama.crawler.Crawler;
-import in.nimbo.moama.kafka.MoamaProducer;
 
-public class CrawlerApp {
+public class App {
     public static void main(String[] args) {
         Initializer.initialize();
         Thread crawl = new Thread(new Crawler());
